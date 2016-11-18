e2c16468dce5c496b5ccdd05b02da3322718f751
Legras Sophie <sophie.legras@insa-lyon.fr>
2016-11-18 22:40:42 +0000

    Phong model ok

--- main.cpp
+++ main.cpp
@@ -251,7 +251,7 @@ void display() {
 	// tell GL to only draw onto a pixel if the shape is closer to the viewer
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
 	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgramID);
 
